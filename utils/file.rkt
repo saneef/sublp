89f1988baea7b873c87529162812e3d86210b403
Saneef Ansari <hello@saneef.com>
2023-07-09 01:07:10 +0530

    feat: add file utils

---
+++ utils/file.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(require rackunit)
+
+(provide build-absolute-path
+         build-absolute-directory-path
+         equal-path?)
+
+(define (build-absolute-path p)
+  (simplify-path (path->complete-path (expand-user-path p))))
+
+(define (build-absolute-directory-path p)
+  (simplify-path (path->directory-path (expand-user-path p))))
+
+; (or String Path) (or String Path) -> Boolean
+; Checks if two paths are equal. Works with user path and trailing slash paths
+(define (equal-path? p1 p2)
+  (let ([v1 (build-absolute-directory-path p1)] [v2 (build-absolute-directory-path p2)])
+    (equal? v1 v2)))
+
+(module+ test
+  (check-true (equal-path? "/root" "/root"))
+  (check-true (equal-path? (build-path "/root") (build-path "/root")))
+  (check-true (equal-path? "/root" "/root/")))
